7ab2e079298f3961286b2805189c78bebad00c8b
adm00008 <adm00008@mix.wvu.edu>
2023-01-11 13:14:12 -0500

    <Adam> Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,1 +1,24 @@
 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
+library(swirl)
+5 + 7
+x <- 5 + 7
+x
+y <- x -3
+y
+c(1.1, 9, 3.14)
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+my_div <- z/my_sqrt
+1
+my_div
+c(1, 2, 3, 4) +c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 + 1000
+my_div
+2
